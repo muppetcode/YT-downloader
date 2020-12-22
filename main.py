0acb5b43a4e0f5738103d9e0b6f6b2e2311b5104
muppetcode <alexheathcote1@btinternet.com>
2020-12-22 12:48:24 +0000

    Cleanup and resolution revamp

--- main.py
+++ main.py
@@ -11,15 +11,66 @@ sucsessful_videos = list() # List that stores finsihed downloads
 watermark = Label(root, text="YouTube Downloading Tool by muppet")
 watermark.pack()
 
-entry_field = Entry(root) # URL entry widget
-entry_field.pack()
-entry_field.insert(0, "Copy URL here")
-
-queue_label = Label(root, text="Videos: 0")
-queue_label.pack()
-
 def set_queue_label():
     queue_label.config(text="Videos: {}".format(len(video_list)))
+
+def get_itag_value(itag):
+    global itag_value
+    itag_value = itag
+    remove_start_page()
+    create_download_page()
+
+def create_download_page():
+    global add_button
+    global clear_button
+    global download_button
+    global queue_label
+    global entry_field
+    add_button = Button(root, text="Add to queue", command=add_video)
+    clear_button = Button(root, text="Clear", command=clear_queue)
+    download_button = Button(root, text="Download", command=download_videos)
+    entry_field = Entry(root) # URL entry widget
+    queue_label = Label(root, text="Videos: 0")
+    entry_field.pack()
+    queue_label.pack()
+    add_button.pack()
+    clear_button.pack()
+    download_button.pack()
+
+def remove_download_page():
+    entry_field.destroy()
+    queue_label.destroy()
+    add_button.destroy()
+    clear_button.destroy()
+    download_button.destroy()
+
+def create_start_page():
+    global resolution_button_18
+    global resolution_button_22
+    global resolution_button_137
+    resolution_button_18 = Button(root, text="Resolution Itag: 18", command=lambda: get_itag_value(18))
+    resolution_button_22 = Button(root, text="Resolution Itag: 22", command=lambda: get_itag_value(22))
+    resolution_button_137 = Button(root, text="Resolution Itag: 137", command=lambda: get_itag_value(137))
+    resolution_button_18.pack()
+    resolution_button_22.pack()
+    resolution_button_137.pack()
+
+def remove_start_page():
+    resolution_button_18.destroy()
+    resolution_button_22.destroy()
+    resolution_button_137.destroy()
+
+def remove_log():
+    try:
+        finish_label.destroy()
+        log_label.destroy()
+        finished_log_label.destroy()
+        continue_button.destroy()
+    except:
+        finish_label.destroy()
+        finished_log_label.destroy()
+        continue_button.destroy()
+    create_start_page()
 
 def add_video():
     if entry_field.get() == "" or entry_field.get() == "Copy URL here": # Checks that the entry isn't blank or the defualt value
@@ -39,29 +90,28 @@ def download_videos(): # I wouldn't change this too much
     for x, video in enumerate(video_list):
         try:
             v = pytube.YouTube(video) # pytube video object
-            stream = v.streams.get_by_itag(22) # Only compatible resolution atm :(
+            stream = v.streams.get_by_itag(itag_value)
             stream.download("Videos") # Places all videos in the 'Video' folder, feel free to change this
+            global finish_label
             finish_label = Label(root, text=("Finished video {}".format(x)))
             finish_label.pack()
-            video_list.clear()
             set_queue_label()
             sucsessful_videos.append(video)
         except: # Python won't give a code error on invalid URLs or videos
+            global log_label
             log_label = Label(root, text="Invalid URL or incompatible resolution")
             log_label.pack()
+        global finished_log_label
         finished_log_label = Label(root, text="Downloaded {} out of {} videos".format(len(sucsessful_videos), len(video_list)))
         finished_log_label.pack()
         video_list.clear()
         sucsessful_videos.clear()
         set_queue_label()
+        remove_download_page()
+        global continue_button
+        continue_button = Button(root, text="Continue", command=remove_log)
+        continue_button.pack()
 
-add_button = Button(root, text="Add to queue", command=add_video)
-add_button.pack()
-
-clear_button = Button(root, text="Clear", command=clear_queue)
-clear_button.pack()
-
-download_videos = Button(root, text="Download", command=download_videos)
-download_videos.pack()
+create_start_page()
 
 root.mainloop()
